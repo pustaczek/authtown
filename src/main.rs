6ff4febbf2c4c07b3a9bad1b90581bdd5fff75b5
Mateusz Cegiełka <mateusz@cegla.net>
2021-09-30 17:17:03 +0200

    :sparkles: Add basic Hyper web server

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,29 @@
-fn main() {}
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+fn main() {
+    run_async().unwrap();
+}
+
+fn run_async() -> Result<(), Box<dyn std::error::Error>> {
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_io()
+        .build()?;
+    runtime.block_on(run())
+}
+
+async fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let address = SocketAddr::from(([127, 0, 0, 1], 8000));
+    let service_factory =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello_world)) });
+    let server = Server::bind(&address).serve(service_factory);
+    server
+        .await
+        .map_err(|err| Box::new(err) as Box<dyn std::error::Error + 'static>)
+}
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello, world!".into()))
+}
